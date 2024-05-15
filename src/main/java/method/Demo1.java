657bbe8ec2a48e56bc44452e5c2b09e838487eb9
czy <chenziyun@stu.ouc.edu.cn>
2024-05-15 09:57:21 +0800

    联系查看字节码文件，分析程序运行

---
+++ src/main/java/method/Demo1.java
@@ -0,0 +1,17 @@
+package method;
+
+/**
+ * ClassName: Demo1
+ * Package: method
+ * Description:
+ *      使用字节码文件，分析i=i++；的结果为
+ * @Author Chen Ziyun
+ * @Version 1.0
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        int i = 0;
+        i = i++;
+        System.out.println(i);
+    }
+}
